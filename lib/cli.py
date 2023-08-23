10f967cc5273f08a03be54fb31739b19c185b5e8
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-08-23 06:49:28 -0600

    displays the user selected recipe

--- lib/cli.py
+++ lib/cli.py
@@ -12,7 +12,8 @@ def displayrecipe(recipe_id):
     # Click sends all arguments as tuple of strings.  They need to be converted to
     # integers
     click.echo(f"The recipe id is {recipe_id}")
-    Helper.get_selected_recipe(int(recipe_id))
+    recipe_info = Helper.get_selected_recipe(int(recipe_id))
+    display_recipe(recipe_info)
 
 @click.option("-a", "--alpha", is_flag = True, show_default = True, default = 0)
 @cli.command()
@@ -22,6 +23,45 @@ def listrecipes(alpha):
     display_recipe_list(recipe_list)
 
 
+
+def display_recipe(recipe_info):
+    # Print the recipe title
+    click.echo(f"Recipe Title:  {recipe_info[0][0]}")
+    click.echo("\nIngredients")
+#    click.echo(recipe_info[1])
+    display_recipe_ingredients(recipe_info[1])
+    click.echo("\nInstructions")
+    display_recipe_instructions(recipe_info[2])
+
+#
+# The ingredients_info is a data structure that is a list of tuples containing the ingredient information.
+# [(amount, mesurement_unit, ingredient),...].  The amount and measurement unit are right justified in the
+# leftmost 9 columns of the display (including a space pad for the ingredient)
+#
+# The format accomodates non-unit ingredients.  For example, if the recipe calls for 5 eggs,then
+#   ingredient_info[0] == 5, ingredient_info[1] == None, ingredient_info[2] == "eggs"
+# In this scenario, there is nothing in the 9 columns of measurement info, and the amount is joined with the
+# ingredient
+def display_recipe_ingredients(ingredients_info):
+    for ingredient in ingredients_info:
+    
+        if ingredient[1] == None:
+            ingredient_string = " "*9 + ingredient[0] + " " + ingredient[2]
+        else:
+            ingredient_string = ("%9s" % (ingredient[0] + " " + ingredient[1] + " ")) + ingredient[2]
+        click.echo(ingredient_string)
+
+#
+# instruction_info is a list of tuples that contain the step by step instructins for the recipe.
+# [(instruction1,), (instruction2,), ... (instructionn,)]
+def display_recipe_instructions(instruction_info):
+    instruction_number = 1
+    for instruction in instruction_info:
+        # instruction is a single element tuple, so display instruction[0]
+        click.echo(f"{instruction_number}.  {instruction[0]}")
+        instruction_number += 1
+        
+        
 def display_recipe_list(recipe_list):
     for recipe_item in recipe_list:
         display_string = "  ".join( (str(recipe_item[0]),recipe_item[1]) )
--- lib/helpers.py
+++ lib/helpers.py
@@ -28,8 +28,8 @@ class Helper:
     # contains the instructions, and finally the 3 contains all the ingredient information
     #
     # ( [id, recipe_title, recipe_food_pyramid_info)],
-    #   [(instruction_1,), (instruction_2,),...(instruction_n,)),
-    #   [(ingredient_info_1), (ingredient_info_2),...(ingredient_info_n)] )
+    #   [(ingredient_info_1), (ingredient_info_2),...(ingredient_info_n)],
+    #   [(instruction_1,), (instruction_2,),...(instruction_n,)]
     # )
     def get_selected_recipe(recipe_id):
         engine = create_engine('sqlite:///db/recipes.db')
@@ -39,8 +39,15 @@ class Helper:
         recipe_info = session\
             .query(Recipe.recipe_title, Recipe.vegetables_and_fruit, Recipe.breads_and_cereals, Recipe.dairy, Recipe.meat, Recipe.fats_and_sugar )\
             .filter(Recipe.id == recipe_id).all()
-        print(recipe_info)
+        
         instruction_info = session.query(Instruction.instruction).filter(Instruction.recipe_id == recipe_id).all()
-        print(instruction_info)
-        ingredient_info = session.query(Ingredient.measurement_amount, Ingredient.measurement_unit, Ingredient.ingredient).filter(Ingredient.recipe_id == recipe_id).all()
-        print(ingredient_info)
+        ingredient_info = session.query(Ingredient.measurement_amount, Ingredient.measurement_unit, Ingredient.ingredient)\
+            .filter(Ingredient.recipe_id == recipe_id).all()
+     
+        # The recipe_info has a single tuple embedded in a list (i.e. [(recipe_title, food_pyramid_info1,...)]).  Extract the tuple from the 
+        # list and make that the 0th element of the return tuple.  In short, that's why there is recipe_info[0] instead of recipe_info
+        return_info = (recipe_info[0], ingredient_info, instruction_info)
+     #   print(return_info)
+
+
+        return((recipe_info[0], ingredient_info, instruction_info))
